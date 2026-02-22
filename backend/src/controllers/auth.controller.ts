236ad1b709bb8aff607ef25d64fe90b230a04a2d
Prithivi <prithivi@users.noreply.github.com>
2026-02-22 23:10:17 +0545

    feat: implement signIn controller and integrate signInService

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import logger from '@/lib/logger.lib.js';
 import type { Request, Response } from 'express';
 import successResponse from '@/utils/success-response.util.js';
-import { signUpService } from '@/services/auth.service.js';
+import { signUpService, signInService } from '@/services/auth.service.js';
 
 export const signUpController = async (req: Request, res: Response) => {
   const { user } = await signUpService(req.body);
@@ -12,4 +12,15 @@ export const signUpController = async (req: Request, res: Response) => {
   });
 
   successResponse(res, 201, 'User registered successfully', { user });
+};
+
+export const signInController = async (req: Request, res: Response) => {
+  const { user, token } = await signInService(req.body);
+
+  logger.info(`User signed in with email: ${user.email}`, {
+    label: 'AuthController',
+    userId: user.id,
+  });
+
+  successResponse(res, 200, 'User signed in successfully', { user, token });
 };
