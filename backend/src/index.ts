d7e0b6da0f624edbc1a7b0e427dafce2c9ba907a
Prithivi <prithivi@users.noreply.github.com>
2026-02-22 20:17:55 +0545

    feat: set up Express server with middleware for JSON, URL-encoded data, cookies, and CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Backend running!');
+import 'dotenv/config';
+import app from '@/app.js';
+import logger from '@/lib/logger.lib.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`Server is running in http://localhost:${PORT}`, {
+    label: 'Server',
+  });
+});
