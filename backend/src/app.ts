b56dddb6068e5cc8e18ac292aee155b36e4eefed
Prithivi <prithivi@users.noreply.github.com>
2026-02-22 22:49:56 +0545

    feat: add global error handler middleware to the application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { type Express } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import corsOptions from './config/cors.config.js';
+import globalErrorHandler from './middleware/global-error.middleware.js';
 
 const app: Express = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
+
+app.use(globalErrorHandler);
 
 export default app;
