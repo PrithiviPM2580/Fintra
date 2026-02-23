f2e8fb60362e8682a71c33380ca7ac5c860247ff
Prithivi <prithivi@users.noreply.github.com>
2026-02-23 18:43:26 +0545

    feat: implement cookie utility for setting and clearing cookies

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import logger from '@/lib/logger.lib.js';
 import type { Request, Response } from 'express';
 import successResponse from '@/utils/success-response.util.js';
 import { signUpService, signInService } from '@/services/auth.service.js';
+import cookie from '@/lib/cookie.lib.js';
 
 export const signUpController = async (req: Request, res: Response) => {
   const { user } = await signUpService(req.body);
@@ -17,9 +18,12 @@ export const signUpController = async (req: Request, res: Response) => {
 export const signInController = async (req: Request, res: Response) => {
   const { user, token } = await signInService(req.body);
 
+  cookie.set(res, 'token', token);
+
   logger.info(`User signed in with email: ${user.email}`, {
     label: 'AuthController',
     userId: user.id,
+    token,
   });
 
   successResponse(res, 200, 'User signed in successfully', { user, token });
