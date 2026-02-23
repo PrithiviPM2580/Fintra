9076f8d934118dd91698a690c7b46f783cc84739
Prithivi <prithivi@users.noreply.github.com>
2026-02-23 22:17:00 +0545

    feat: add token verification function with error handling for invalid tokens

--- backend/src/lib/jwt.lib.ts
+++ backend/src/lib/jwt.lib.ts
@@ -1,5 +1,6 @@
 import type { JWTSignOptions, Payload } from '@/types/index.js';
 import jwt, { type Secret } from 'jsonwebtoken';
+import APIError from './api-error.lib.js';
 
 const JWT_SECRET: Secret = process.env.JWT_SECRET!;
 
@@ -8,4 +9,19 @@ export const generateToken = (
   options?: JWTSignOptions,
 ): string => {
   return jwt.sign(payload, JWT_SECRET, options);
+};
+
+export const verifyToken = (token: string): Payload => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as Payload;
+    return decoded;
+  } catch (error) {
+    throw new APIError(401, 'Invalid token', true, {
+      details: [
+        {
+          message: 'The provided token is invalid or has expired',
+        },
+      ],
+    });
+  }
 };
